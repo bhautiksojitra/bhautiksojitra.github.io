ae0e833e42601b53d01637dd2854c459b91d5c6b
Bhautik Sojitra <bhautiksojitra73@gmail.com>
2024-11-25 18:49:57 -0600

    updated jobs page

--- jobs/job1.md
+++ jobs/job1.md
@@ -14,8 +14,27 @@ tech_stack:
 
 
 <ul style="list-style-type: circle;">
-    <li> Implemented a .NET MAUI mobile app for embedded control systems, eliminating reliance on laptops, and increasing field operations efficiency by 30% </li>
-    <li> Ensured 99% user satisfaction through 24x7 support for .NET Core web applications and comprehensive documentation of critical user processes </li>
-    <li> Migrated from NancyFx to ASP.NET, improving CPU performance by 50% </li>
-    <li> Centralized file access and enhanced accessibility for 100+ users by integrating AWS S3 with the mobile app </li>
+    <li> Project #1: A .NET and VueJS based web app that provides UI to control systems. Deployed on Windows and Linux.
+        <ul>
+            <li>Optimize UI performance </li>
+            <li>Reduce CPU usage by 50% on Linux</li>
+            <li>Migrate from NancyFx hosting to ASP.NET</li>
+            <li>Provide bugfixes and new features for 1.5+ years</li>
+        </ul>
+    </li>
+    <li> Project #2: Mobile app project based in .NET MAUI also designed to interface with control systems, providing easy access in the field. 
+        <ul> 
+            <li> Set up Jenkins pipeline for CI/CD </li> 
+            <li> Configure Play Console (Android) and App Store Connect (iOS) for deploying app bundles </li> 
+            <li> Implement general features while working with 2 other teammates </li> 
+            <li> Work a little bit with AWS S3 to upload and download user/application level files </li>
+        </ul>
+    </li>
+    <li> Project #3: Web app based in AWS Amplify to streamline the process of remote support
+         <ul>
+            <li> Create and read DynamoDB tables and forward data through API gateway </li>
+            <li> Utilize the concept of Amplify environments to work in isolation and reduce conflicts with team members </li>
+            <li> Make use of various AWS services like ECS, SES, and Cognito </li>
+         </ul>  
+    </li>
 </ul>
--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -3,6 +3,9 @@ import Layout from '../components/layout.js'
 import { graphql, useStaticQuery } from 'gatsby';
 import Markdown from 'markdown-to-jsx';
 import Headline from '../components/headline.js';
+import JobData from '../data/jobData.json';
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
 const JobsPage = () => {
 
     const data = useStaticQuery(graphql`
@@ -29,6 +32,14 @@ const JobsPage = () => {
     )
     const tabs = data.allMarkdownRemark.edges;
     const [activeTab, setActiveTab] = useState(0);
+
+    // State to track which dropdown item is expanded
+  const [expandedIndex, setExpandedIndex] = useState(null);
+
+  // Toggle function to expand/collapse details
+  const toggleExpand = (index) => {
+    setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
     
     useEffect(() => {
         setActiveTab(0);
@@ -36,38 +47,56 @@ const JobsPage = () => {
     
     return (
             <div className='flex flex-col w-screen items-center mt-24'>  
-                <div className='flex flex-col w-2/3  min-w-96'>
+                <div className='flex flex-col w-1/2  min-w-96'>
                     <Headline placeholder="Where I've Worked"/>
                     <ul class="flex mt-5">
-                        {tabs.map((tab) => (
+                        {JobData.map((job, index) => (
                             <li className={`border-b mb-2 text-center hover:bg-slate-100 transition-colors duration-200
-                                            ${activeTab === tab.node.frontmatter.id ? "border-b-4 border-b-red-950" : "border-b-2"}`}>
-                                <a
-                                key={tab.node.frontmatter.id}
-                                onClick={() => setActiveTab(tab.node.frontmatter.id)}
+                                            ${activeTab === index ? "border-b-4 border-b-red-950" : "border-b-2"}`}>
+                                <button
+                                key={index}
+                                onClick={() => setActiveTab(index)}
                                 className={`inline-block whitespace-nowrap px-4 py-2 font-medium 
-                                    ${activeTab === tab.node.frontmatter.id ? "text-red-800" : "text-gray-600"} 
+                                    ${activeTab === index ? "text-red-800" : "text-gray-600"} 
                                     transition-colors duration-200`}>
-                                {tab.node.frontmatter.company}
-                                </a>
+                                {job.CompanyName}
+                                </button>
                             </li>
                         ))}
                     </ul>
 
                     <div>
-                        <p className='mb-2 mt-2'>{tabs[activeTab].node.frontmatter.position_name} @ {tabs[activeTab].node.frontmatter.company_fullname}</p>
-                        <p className='text-sm mb-2'>{tabs[activeTab].node.frontmatter.duration}</p>
-                        <Markdown  className='prose'> 
-                            {tabs[activeTab].node.html} 
-                        </Markdown>
-                        {tabs[activeTab].node.frontmatter.tech_stack.map((title) => (
-                            <div class="border px-3 mt-3 mx-1 inline-flex rounded-full"> 
-                                {title[0]} 
+                        <p className='mb-2 mt-2'>{JobData[activeTab].JobTitle} @ {JobData[activeTab].CompanyName}</p>
+                        <p className='text-sm mb-2'>{JobData[activeTab].Duration}</p>
+                        
+                        {JobData[activeTab].JobDescription.map((description, index) => (
+                            <div key={index}>
+                            <div
+                              className="p-3 border mb-2 flex cursor-pointer hover:bg-slate-500"
+                              onClick={() => toggleExpand(index)}
+                            >
+                              <div className="mr-5">
+                                {expandedIndex === index ? (
+                                  <IoIosArrowUp size={30} />
+                                ) : (
+                                  <IoIosArrowDown size={30} />
+                                )}
+                              </div>
+                              <div className="font-bold">{description.Title}</div>
                             </div>
+                            {expandedIndex === index && (
+                              <ul className="pl-8 mb-5 list-disc text-sm text-gray-700 space-y-2">
+                              {description.Details.map((detail, i) => (
+                                <li key={i}>{detail}</li>
+                              ))}
+                            </ul>
+                            )}
+                          </div>
                         ))}
+
                     </div>
                 </div>
-            </div>
+                </div>
     )
 }
 
